245b4d735ed65a23d25da3d987fa8731e63129d6
Lawrence Surges <surgeslc@gmail.com>
2017-06-15 16:16:26 +0000

    Array filtering completed!

--- array-filtlering.js
+++ array-filtlering.js
@@ -1,8 +1,8 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var filtered = numbers.filter(evenNumbers);
 
   function evenNumbers(numbers) {
     return (numbers % 2 === 0);
   }
 
+var filtered = numbers.filter(evenNumbers);
 console.log(filtered);
--- wip.js
+++
@@ -1,6 +0,0 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var filtered = numbers.filter(function (evenNumbers) {
-
-  function evenNumbers(numbers) {
-    return numbers % 2 === 0;
-  }
